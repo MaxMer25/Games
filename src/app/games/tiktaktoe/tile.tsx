79aa23ed3be2821f0f65e1b62509cd0c082f48f3
F1ubbY <max-mergenthal@gmx.de>
2023-03-28 15:56:40 +0200

    added "X" mark

--- src/app/games/tiktaktoe/tile.tsx
+++ src/app/games/tiktaktoe/tile.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import styles from "./styles.module.css";
+import { useState } from "react";
 
 export default function Tile() {
+  const [marked, setMarked] = useState(false);
+
+  function handleClick() {
+    setMarked(!marked);
+  }
   return (
     <>
-      <div className={styles.tile}></div>
+      <div onClick={handleClick} className={styles.tile}>
+        <span>{marked ? "X" : ""}</span>
+      </div>
     </>
   );
 }
