f78509b0738990ace925cb9870ca652c4e11581f
Jurian Mergenthal <jurian.mergenthal@celebrate.company>
2023-04-04 10:19:47 +0200

    feat: update db model, remove unused api methods

--- prisma/tile.ts
+++ prisma/tile.ts
@@ -1,24 +1,7 @@
 import prisma from "./prisma";
 
 // READ
-export const getAllTiles = async () => {
-  const tiles = await prisma.tile.findMany({});
-  return tiles;
-};
-
-export const getTiles = async (id: string) => {
-  const tiles = await prisma.tile.findUnique({
-    where: { id },
-  });
-  return tiles;
-};
-
-// CREATE
-export const createTile = async (marked: boolean) => {
-  const tile = await prisma.tile.create({
-    data: {
-      marked,
-    },
-  });
-  return tile;
+export const getGameState= async () => {
+  const game = await prisma.ticTacToe.findMany({});
+  return game;
 };
--- src/app/api/tiles/route.ts
+++ src/app/api/tiles/route.ts
@@ -1,17 +1,7 @@
-import { getAllTiles, getTiles } from "../../../../prisma/tile";
+import { getGameState } from "../../../../prisma/tile";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
-  if (id) {
-    // Get a single tile if id is provided is the query
-    // api/tiles?id=1
-    const tile = await getTiles(id);
-    return NextResponse.json(tile);
-  } else {
-    // Otherwise, fetch all tiles
-    const tiles = await getAllTiles();
-    return NextResponse.json(tiles);
-  }
+export async function GET() {
+    const gameState = await getGameState();
+    return NextResponse.json(gameState);
 }
